8ee402c06256aa80a74a18234576e8a866f9f353
Augusto Souza <augustodesouzacardoso@gmail.com>
2024-08-02 08:19:12 -0300

    Create SendMessageComponent. Add sassc-rails gem to solve console error loading CSS stylesheets and change all .css files to .scss. A few more styling.

---
+++ test/components/send_message_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SendMessageComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(SendMessageComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
