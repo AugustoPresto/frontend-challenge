a87126dfd1ed1d186e2cdcbb0736b4f3264e33a8
Augusto Souza <augustodesouzacardoso@gmail.com>
2024-08-01 23:28:09 -0300

    Not perfect, but now we have the hover feature with Turbo Stream

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index] do
+    member do
+      get :user_profile_card
+    end
+  end
 end
